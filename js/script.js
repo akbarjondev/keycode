0010dce9cdaa6e77d91b0ba3feefb5c104e9f1ee
akbarjondev <tojiyev.akbarjon@gmail.com>
2020-12-02 18:31:42 +0500

    Add press any key

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var elKeycodeNum = elKeycode.querySelector('.keycode__num');
 var elKeycodeKeyboard = elKeycode.querySelector('.keycode__keyboard');
 
 if(document.body.clientWidth < 768) {
-	elKeycode.textContent = 'Only computers!'
+	elKeycode.textContent = 'Only computers!';
 }
 
 document.body.addEventListener('keydown', (evt) => {
